4a55dcf9e9cfea195a2beea19a0aeab75ad445c6
itmustbemimic` <sken9988@gmail.com>
2021-11-02 14:45:47 +0900

    resize size to 300

--- routes/images.js
+++ routes/images.js
@@ -17,7 +17,7 @@ router.post('/', upload.single('aaa'), (req, res) => {
     console.log('몬가... 몬가 일어나고 잇슴...');
 
     sharp(req.file.path)
-        .resize({width:400}) //썸네일용 이미지 리사이즈
+        .resize({width:300}) //썸네일용 이미지 리사이즈
         .toFile('public/images/thumbnail/' + req.file.filename, function (err) {
             if (err) console.error(err);
             else console.log(req.file.filename + 'thumbnail generated!');
